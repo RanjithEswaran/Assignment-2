6bb4b04529d963799b58460ed8aa4ae6700cb227
RanjithEswaran <30902121+RanjithEswaran@users.noreply.github.com>
2017-08-16 13:11:34 +0530

    Challenging Problems

---
+++ EvenFibonacciSeries.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenFibonacciSeries
+{
+    class Program
+    {
+        void Fibonacci(int range)
+        {
+            int a = 0,b=1,c,sum=0;
+            for(int i=2;i<range;i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                if (c % 2 == 0)
+                {
+                    Console.WriteLine("{0} ", c);
+                    sum += c;
+                }
+            }
+            Console.WriteLine("The sum of even terms in Fibonacci series is {0}", sum);
+        }
+        static void Main(string[] args)
+        {
+            int Range;
+            Console.WriteLine("Enter the Range");
+            Range = Convert.ToInt16(Console.ReadLine());
+            Program p = new Program();
+            p.Fibonacci(Range);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MultipleOf3Or5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleOf3Or5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Range;
+            long sum =0;
+            Console.WriteLine("Enter the range");
+            Range = Convert.ToInt16(Console.ReadLine());
+            for(int i=1;i<=Range;i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                    sum += i;
+                else if (i % 3 == 0)
+                    sum += i;
+                else if (i % 5 == 0)
+                    sum += i;
+            }
+            Console.WriteLine("The sum of multiples of 3 and 5 within the given range is {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
